807abf05ad8f08d82caf4592a23a6e8078a6dadd
TheBrioche <115082519+TheBrioche@users.noreply.github.com>
2024-01-29 12:33:49 +0000

    css fix

--- api/index.php
+++ api/index.php
@@ -36,6 +36,74 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
   <title>Candy Crush Hacks 2024</title>
+  <style>
+    @font-face {
+  font-family: 'Comic Sans MS'; /* Choose a name for your font */
+  src: url('comic-sans-ms/COMIC.TTF') format('woff2'); /* Path to your font file */
+  /* You can include additional properties like font-weight, font-style, etc. */
+}
+
+
+* {
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+}
+input {
+  text-align: center;
+}
+
+body {
+   font-family: 'Comic Sans MS', sans-serif;'
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   height: 100vh;
+   background: #2b2b2b;
+   color: #fff;
+   background-image: url('https://helios-i.mashable.com/imagery/articles/00OWdWk89IkIaNw4UMche72/hero-image.fill.size_1248x702.v1698407968.jpg');
+    background-size: cover;
+}
+.container {
+
+}
+
+  i {
+    color: white;
+    font-family: "Comic Sans MS", sans-serif;
+    text-align: left;
+  }
+
+  .ab {
+    display: block;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    text-decoration: none;
+    margin: 20px;
+  }
+
+  /* Rainbow text */
+  .rainbow {
+    background-image: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet);
+    background-size: 400%;
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    animation: rainbow 10s linear infinite;
+  }
+
+  /* Animation for rainbow text */
+  @keyframes rainbow {
+    0% {
+      background-position: 0%;
+    }
+    100% {
+      background-position: 400%;
+    }
+  }
+  </style>
 </head>
 <body>
   <script type="module">
